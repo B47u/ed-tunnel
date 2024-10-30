6742a814326b3d053101bd5a691b1e9572d6a48d
B47u <147147938+B47u@users.noreply.github.com>
2024-10-31 04:49:16 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '217ddf65-5ca4-4b41-babe-aacc677ffe09;
+let userID = '217ddf65-5ca4-4b41-babe-aacc677ffe09';
 
 const พร็อกซีไอพีs = ['178.128.91.137'];
 
